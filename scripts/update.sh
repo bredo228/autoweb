3b3d15a9af9d88efec78752a25ca0ed3ad2f4bac
bredo <bredo@bredo.tech>
2025-02-16 10:39:55 +1300

    feat: add private repository support

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,8 +2,19 @@
 
 cd /srv/
 
-if [ -d ".git" ]; then
-	git pull
+if [ "${PRIVATE_REPOSITORY}" = "true" ]; then
+	# Repository is private, treat it as such.	
+	if [ -d ".git" ]; then
+		git pull https://${GIT_USERNAME}:${GIT_TOKEN}@${GIT_REPOSITORY#https://} # thanks sveken
+	else
+		git clone https://${GIT_USERNAME}:${GIT_TOKEN}@${GIT_REPOSITORY#https://} .
+	fi
+
 else
-	git clone ${GIT_REPOSITORY} .
+	# we don't need creds, just send it.
+	if [ -d ".git" ]; then
+		git pull
+	else
+		git clone ${GIT_REPOSITORY} .
+	fi
 fi
